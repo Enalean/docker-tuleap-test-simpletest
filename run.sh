a0f02008ea723124e66f20e1a576175969ebb3e5
Manuel Vacelet <manuel.vacelet@enalean.com>
2014-07-31 10:01:11 +0200

    Add info

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 service mysqld start
-#service httpd start
-
-#make -C /tuleap TULEAP_LOCAL_INC=/etc/integration_tests.inc OUTPUT_DIR=/output BUILD_ENV=ci PHPUNIT_OPTIONS="$@" ci_simpletest
 
 JOB_NAME=ut /tuleap/tools/continuous_integration/ci_build.sh --workspace=/ --srcdir=tuleap
+
+echo "Completed"
